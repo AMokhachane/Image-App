50d3fa0a47629495a96723b8114958c21b73d9fe
Amanda Mokhachane <AMokhachane@singular.co.za>
2024-07-17 07:56:10 +0200

    Changes before Outlook

--- api/EmailService/EmailSender.cs
+++ api/EmailService/EmailSender.cs
@@ -9,11 +9,11 @@ namespace api.EmailService
     {
         public async Task SendEmailAsync(string email, string subject, string message)
         {
-            using (var client = new SmtpClient("localhost", 1025)) // MailHog SMTP server address and port
+            using (var client = new SmtpClient("smtp-mail.outlook.com", 587)) // MailHog SMTP server address and port
             {
                 var mailMessage = new MailMessage
                 {
-                    From = new MailAddress("amandamokhachane88@gmail.com"), // Replace with your sender email address
+                    From = new MailAddress("AmandaMokhachane88@outlook.com"), // Replace with your sender email address
                     Subject = subject,
                     Body = message,
                     IsBodyHtml = true // Set to true if your message body is in HTML format
