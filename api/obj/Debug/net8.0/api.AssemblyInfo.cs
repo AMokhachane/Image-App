e56f3093fd8a6fe57faf2dd62db2493d9df9cb9a
Amanda Mokhachane <AMokhachane@singular.co.za>
2024-07-23 14:52:19 +0200

    Last commit before adding password validations

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8e38085886bdfe61e5ea40a559914aa7a3b1077")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9b544470dfbea955d777355a6739956a54b1090")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/ResetPassword.js
+++ src/ResetPassword.js
@@ -1,15 +1,21 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import ResetCSS from './ResetPassword.module.css'; // Import the CSS module
 
 const ResetPassword = () => {
     const [email, setEmail] = useState('');
     const [newPassword, setNewPassword] = useState('');
-    
+    const [confirmPassword, setConfirmPassword] = useState('');
     const [message, setMessage] = useState('');
     const [error, setError] = useState('');
 
     const handleSubmit = async (event) => {
         event.preventDefault();
+
+        if (newPassword !== confirmPassword) {
+          setError('Passwords do not match');
+          return;
+        }
 
         try {
             const response = await axios.post('http://localhost:5205/api/account/ResetPasswordByEmail', {
@@ -30,32 +36,53 @@ const ResetPassword = () => {
     };
 
     return (
-        <div>
-            <h2>Reset Password</h2>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label>Email:</label>
-                    <input
-                        type="email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        required
-                    />
+        <div className={ResetCSS['register-container']}>
+          <div className={ResetCSS['wrapper']}>
+            <div className={ResetCSS['form-container']}>
+              <form onSubmit={handleSubmit} className={ResetCSS['register-form']}>
+                <h1>Reset Password</h1>
+                {error && <div className={ResetCSS['error']}>{error}</div>}
+                <div className={ResetCSS['form-group']}>
+                  <label>Email Address</label>
+                  <input
+                    type="email"
+                    id="email"
+                    placeholder="Enter Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                  />
                 </div>
-                <div>
-                    <label>New Password:</label>
-                    <input
-                        type="password"
-                        value={newPassword}
-                        onChange={(e) => setNewPassword(e.target.value)}
-                        required
-                    />
+                <div className={ResetCSS['form-group']}>
+                  <label>New Password</label>
+                  <input
+                    type="password"
+                    value={newPassword}
+                    placeholder="Enter new Password"
+                    onChange={(e) => setNewPassword(e.target.value)}
+                    required
+                  />
                 </div>
-               <button type="submit">Reset Password</button>
-            </form>
-            {message && <p style={{ color: 'green' }}>{message}</p>}
-            {error && <p style={{ color: 'red' }}>{error}</p>}
+                <div className={ResetCSS['form-group']}>
+              <label>Confirm Password</label>
+              <input
+                type="password"
+                id="confirmPassword"
+                placeholder="Confirm Password"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                required
+              />
+            </div>
+
+                <button type="submit" className={ResetCSS['register-btn']}>Reset password</button>
+              </form>
+              {message && <p style={{ color: 'green' }}>{message}</p>}
+                {error && <p style={{ color: 'red' }}>{error}</p>}
+            </div>
+          </div>
         </div>
-    );
-};
+      );
+    };
+
 export default ResetPassword;
