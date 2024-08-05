450024ec62ab6fed4ecc20152fcca53296937642
Amanda Mokhachane <AMokhachane@singular.co.za>
2024-08-05 16:03:04 +0200

    last code that works all pages

--- src/components/ImageDetails.js
+++ src/components/ImageDetails.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { useHistory, useLocation } from 'react-router-dom';
 import axios from 'axios';
 import ImageDetailsCSS from './ImageDetails.module.css';
-import { faTag } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const ImageDetails = () => {
   const history = useHistory();
@@ -21,15 +19,12 @@ const ImageDetails = () => {
 
   return (
     <div className={ImageDetailsCSS['image-details-page']}>
+      <button onClick={() => history.goBack()} className={ImageDetailsCSS.backButton}>Back</button>
       <div className={ImageDetailsCSS['image-details-box']}>
-        <button onClick={() => history.goBack()} className={ImageDetailsCSS.backButton}>X</button>
         <div className={ImageDetailsCSS['image-details']}>
           <img src={image.url} alt={image.title} className={ImageDetailsCSS.image}/>
           <div className={ImageDetailsCSS['details']}>
-          <h4 className="name">
-                {image.title}
-                <FontAwesomeIcon icon={faTag} className={ImageDetailsCSS.tag} />
-              </h4>
+            <h4 className="name">{image.title}</h4> {/* Using 'title' */}
             <p className="description">{image.imageDescription}</p> {/* Using 'imageDescription' */}
             <button onClick={() => deleteImage(image.imageId)} className={ImageDetailsCSS.deleteButton}>
               Delete
