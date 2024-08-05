4155c8903047ad6cc706b134a436cf3de1115806
Amanda Mokhachane <AMokhachane@singular.co.za>
2024-08-05 08:20:42 +0200

    IGA 005 DONE, CHANGES BEFORE HOME PAGE

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5d16ec170bca6875b3b6c77e324f90a3a2bf5a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc861a836b52772ca32b1c8f96f13b3a0958b2d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
